6b0e2156a958b69c0ef09f8663080c66e1936dc9
Amir Khan <amirkhan1985@gmail.com>
2023-11-05 23:44:50 +0530

    update

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css'
+import Card from './Card'
+function Home() {
+    return (
+        <>
+            <div className="container">
+            <Card/>
+            <Card/>
+            <Card/>
+            
+            </div>
+        </>
+    )
+}
+
+export default Home;
---
+++ src/components/Nav2.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import './Nav2.css'
+function Nav2(){
+    return(
+        <>
+        <div className="container" style={{backgroundColor:'red'}}>
+         
+          <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Service</a></li>
+            </ul>
+          </nav>
+
+        </div>
+        </>
+    )
+}
+
+export default Nav2;
